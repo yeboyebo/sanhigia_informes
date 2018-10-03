caa75eeaff713623f214fe9d92b601ddef51c7e3
Nikolay-YeboYebo <nikolay.yeboyebo@gmail.com>
2018-10-03 18:11:27 +0200

    Comprobar la herencia de altas_clientes

---
+++ settings.py
@@ -0,0 +1,11 @@
+from os import path
+import sys
+
+PROJECT_ROOT = path.dirname(path.abspath(path.dirname(__file__)))
+
+sys.path.insert(0, path.join(PROJECT_ROOT, "../../../motor/"))
+sys.path.insert(1, path.join(PROJECT_ROOT, "apps/"))
+
+from YBAQNEXT.settings import *
+from YBAQNEXT.yeboapps import *
+from .local import *
